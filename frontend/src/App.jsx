ce2788651b7356832e6dd9ced53eaaa5dcbfe067
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-04-27 19:42:10 +0530

    Dependencies added
    shadcn and tailwndcss added, it's working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      hellow
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
