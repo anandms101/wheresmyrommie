7907ed3a089c96f3169155036b479b67543e22d1
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-04-28 13:40:04 +0530

    Frontend - welcome screen styling added

--- frontend/src/components/pages/welcomeScreen/welcome-screen.jsx
+++ frontend/src/components/pages/welcomeScreen/welcome-screen.jsx
@@ -1,7 +1,6 @@
 import { SignInButton } from "@clerk/clerk-react";
 import TypewriterString from "./typewritter";
 import { Button } from "@/components/ui/button";
-import { ModeToggle } from "../../mode-toggle";
 import Navbar from "../../navbar";
 
 export default function WelcomeScreen() {
@@ -9,11 +8,11 @@ export default function WelcomeScreen() {
     <>
       <Navbar />
       <span className="flex flex-col min-h-screen items-center justify-center">
-        <span
+        <span style={{color: "#49243E"}}
           className="lowercase font-welcomeheading text-5xl font-bold"
         >
           where&apos;s my{" "}
-          <span
+          <span style={{textDecorationColor: "#BB8493"}}
             className="underline"
           >
             roommie
