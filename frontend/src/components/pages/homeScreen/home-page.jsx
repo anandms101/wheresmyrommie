720d48fc926c1d2e87693d41eef411f39436bcbd
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-05-12 13:16:53 +0530

    logic for incomplete profile added on homepage

--- frontend/src/components/pages/homeScreen/home-page.jsx
+++ frontend/src/components/pages/homeScreen/home-page.jsx
@@ -13,19 +13,69 @@ export default function HomePage() {
     if (!user) {
       navigate("/");
     }
+    // IsUserProfileIsCompleted();
   }, [user, navigate]);
 
   function navigateToDetailsPage() {
     navigate("/details");
   }
-  
+
+  // check if any user details is missing from mongoDB
+  function IsUserProfileIsCompleted() {
+    if (!user) {
+      return Promise.resolve(false);
+    }
+
+    const userEmail = user.primaryEmailAddress.emailAddress;
+    console.log("home-page userEmail", userEmail);
+
+    return axios
+      .get(`http://localhost:3000/users?q=email=${userEmail}`)
+      .then((res) => {
+        console.log("home-page res", res.data);
+
+        const requiredFields = [
+          "age",
+          "bio",
+          "profilePicture",
+          "occupation",
+          "smokingHabits",
+          "petOwnership",
+          "cleanliness",
+          "sleepSchedule",
+          "desiredMoveInDate",
+          "budget",
+          "location",
+          "interests",
+          "roomNumber",
+        ];
+        console.log(
+          "userProfileCompleted",
+          requiredFields.every((field) => res.data[field])
+        );
+        return requiredFields.every((field) => res.data[field]);
+      })
+      .catch((err) => {
+        console.error("home-page err", err);
+        throw err;
+      });
+  }
+
+  const InCompleteProfile = () => {
+    return (
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <Button onClick={() => navigateToDetailsPage()}>
+          Please complete your profile!
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <>
       <Navbar />
       <SignedIn>
-        <div className="flex flex-col justify-center items-center min-h-screen">
-          <Button onClick={() => navigateToDetailsPage()}>Please complete your profile!</Button>
-        </div>
+        {IsUserProfileIsCompleted() === true ? null : <InCompleteProfile />}
       </SignedIn>
     </>
   );
