ccf5cee64578260fe6cd0afcb9936e4cc9194b7a
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-05-13 16:03:26 +0530

    card layout added

--- frontend/src/components/pages/homeScreen/completeProfile.jsx
+++ frontend/src/components/pages/homeScreen/completeProfile.jsx
@@ -29,33 +29,51 @@ export default function CompleteProfile() {
     axios.get(`http://localhost:3000/users?email=${userEmail}`).then((res) => {
       console.log("home-page card data", res.data);
       setUserData(res.data);
-      console.log("userData", userData);
+      console.log("userData", res.data);
     });
   }
 
   const CardComponent = (props) => {
+    const user = props.user;
     return (
       <>
-        <Card>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <div className="flex m-4">
+          <Card>
+            <CardHeader>
+              <CardTitle>{user.firstName}</CardTitle>
+              <CardDescription>#{user.roomNumber}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{user.occupation}</p>
+              <p>{user.age}</p>
+              <p>{user.bio}</p>
+              <p>{user.smokingHabits}</p>
+              <p>{user.petOwnership}</p>
+              <p>{user.cleanliness}</p>
+              <p>{user.sleepSchedule}</p>
+              <p>{user.desiredMoveInDate}</p>
+              <p>{user.budget}</p>
+              <p>{user.location}</p>
+              <p>{user.interests}</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
       </>
     );
   };
 
   return (
     <>
-      {userData &&
-        userData.map((user) => <CardComponent key={user._id} user={user} />)}
+      <div className="flex flex-row mt-4">
+        {userData &&
+          userData.map((user) => {
+            console.log("userProp", user);
+            return <CardComponent key={user._id} user={user} />;
+          })}
+      </div>
     </>
   );
 }
